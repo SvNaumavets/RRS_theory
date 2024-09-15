5233f340669a6c906d1544ad8d7354879bc8978d
unknown <sv.naumavets@gmail.com>
2024-09-15 23:42:22 +0300

    Lesson_1

---
+++ lesson1.py
@@ -0,0 +1,28 @@
+print('ex 2')
+print('Hello world!')
+
+# ex 3
+print('ex 3')
+num1 = 100
+name = 'Алексей'
+first_number = 1
+text = 'Мой текст'
+phone_number = '3453423'
+
+# ex 4
+print('ex 4')
+my_name = "Sviatlana"
+print(f'Привет, меня зовут {my_name}.')
+print('Привет, меня зовут', my_name + '.')
+
+# ex 5
+print('ex 5')
+name = input("Input name ")
+surname = input("Input surname ")
+print(f"Привет, меня зовут {name.capitalize()} {surname.capitalize()}.")
+
+# ex 6
+print('ex 6')
+a = int(input())
+b = int(input())
+print(f'a + b = {a + b}')
